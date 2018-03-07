96d99a1870e458aead2b7b9971d9187c5b653f9d
Bro Qiang <broqiang@qq.com>
2018-03-07 23:45:06 +0800

    支持上传到七牛云

--- src/BroImage.php
+++ src/BroImage.php
@@ -1,7 +1,11 @@
 <?php
 namespace BroQiang\LaravelImage;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Intervention\Image\Facades\Image;
+use Qiniu\Auth;
+use Qiniu\Storage\UploadManager;
 
 class BroImage
 {
@@ -9,68 +13,223 @@ class BroImage
 
     protected $root_folder;
 
+    protected $folder;
+
+    protected $file_prefix = '';
+
+    protected $max_width = false;
+
+    protected $upload_type = 'local';
+
+    protected $date_format = 'Y/m/d';
+
+    protected $max_size;
+
+    protected $is_url;
+
+    protected $url;
+
+    protected $filename;
+
+    protected $extension;
+
+    protected $qiniu_access_key;
+
+    protected $qiniu_secret_key;
+
+    protected $qiniu_bucket;
+
+    protected $qiniu_domain;
+
+    protected $random_length = 20;
+
     public function __construct()
     {
-        $this->allowed_ext = config('bro_image.allowed_ext', ["png", "jpg", "gif", 'jpeg']);
-
-        $this->root_folder = '/' . trim(config('bro_image.root_folder', 'uploads/image'), '/') . '/';
+        $this->initConfig();
     }
 
-    public function upload($file, $folder = null, $filePrefix = null, $maxWidth = false)
+    public function initConfig()
     {
-        $folder = $folder ?: config('bro_image.folder', 'default');
+        $configs = config('bro_image', []);
+        $this->setConfig($configs);
+        if (array_key_exists('root_folder', $configs)) {
+            $this->root_folder = '/' . trim($configs['root_folder'], '/') . '/';
+        }
+    }
 
-        $filePrefix = $filePrefix ?: config('bro_image.file_prefix', '');
+    public function upload($file, $config = [])
+    {
+        // 将自定义配置初始化
+        $this->setConfig($config);
+        switch ($this->upload_type) {
+            case 'local':
 
+                return $this->uploadLocal($file);
+                break;
+            case 'qiniu':
+                return $this->uploadQiniu($file);
+                break;
+        }
+    }
+
+    /**
+     * 上传文件到本地
+     * @param  [type] $file [description]
+     * @return [type] [description]
+     */
+    public function uploadLocal($file)
+    {
         // 格式 uploads/image/$folder/2018/02/02/
-        $folderName = $this->root_folder . trim($folder, '/') . '/' . date(config('bro_image.date_format', 'Y/m/d'), time());
+        $folderName = $this->root_folder . $this->folder . '/' . date($this->date_format, time());
 
         // 将 public 目录的绝对路径和 上面定义的路径拼接到一起
         // 格式: /www/www.broqiang.com/public/uploads/image/$folder/2018/02/02/
         $uploadPath = public_path() . $folderName;
 
-        // 获取文件的后缀名，因图片从剪贴板里黏贴时后缀名为空，所以此处确保后缀一直存在
-        $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
-
         // 拼接文件名
-        $filename = $filePrefix . '_' . str_random(20) . '.' . $extension;
-
-        // 如果上传的不是图片将终止操作
-        if (!in_array($extension, $this->allowed_ext)) {
-            return false;
-        }
+        $filename = $this->getFilename($file);
 
         // 将图片移动到我们的目标存储路径中
         $file->move($uploadPath, $filename);
 
-        if ($maxWidth && $extension != 'gif') {
-            $this->reduceSize($uploadPath . '/' . $filename, $maxWidth);
+        if ($this->max_width && $this->extension != 'gif') {
+            $this->reduceSize($uploadPath . '/' . $filename, $this->max_width);
         }
 
-        if (config('bro_image.is_url') && config('app.url')) {
-            $url = config('bro_image.url', config('app.url'));
+        return $this->getUrlPath($folderName, $filename);
+    }
 
-            return $url . $folderName . '/' . $filename;
+    /**
+     * 上传到七牛云
+     * 详细的内容见官方文档：https://developer.qiniu.com/kodo/sdk/1241/php
+     * @param  [type] $file [description]
+     * @return [type] [description]
+     */
+    public function uploadQiniu($file)
+    {
+        $info = [
+            'success' => 1,
+            'message' => '上传成功',
+            'url'     => '',
+        ];
+
+        $filename = $this->getFilename($file);
+
+        $auth  = new Auth($this->qiniu_access_key, $this->qiniu_secret_key);
+        $token = $auth->uploadToken($this->qiniu_bucket);
+
+        $upManager = new UploadManager();
+
+        list($code, $error) = $upManager->putFile($token, $filename, $file);
+
+        if ($error) {
+            $info['success'] = 0;
+            $info['message'] = $error->message();
+            return $info;
         }
 
-        return $folderName . '/' . $filename;
+        $info['url'] = $this->qiniu_domain . '/' . $filename;
+        return $info;
+    }
+
+    public function validateUpload(Request $request, $field)
+    {
+        $rules = [
+            $field => 'bail|required|file|mimes:' . implode($this->allowed_ext, ',') . '|max:' . $this->max_size,
+        ];
+
+        $messages = [
+            $field . '.file'  => '文件上传失败',
+            $field . '.mimes' => '上传失败，文件类型不允许，请上传常规的图片 ' . implode($this->allowed_ext, ',') . '文件',
+            $field . '.max'   => '上传失败，文件过大，文件大小不得超出 ' . $this->max_size . ' KB。',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            $fail = [
+                'success' => 0,
+                'message' => $validator->errors()->first($field),
+                'url'     => '',
+            ];
+            return $fail;
+        }
+    }
+
+    public function setConfig($config)
+    {
+        if (isset($config) && count($config)) {
+            foreach ($config as $key => $value) {
+                $this->$key = $value;
+            }
+        }
+
+        return $this;
+    }
+
+    public function getUrlPath($folderName, $filename)
+    {
+        $info = [
+            'success' => 1,
+            'message' => '上传成功',
+            'url'     => '',
+        ];
+
+        if ($this->is_url) {
+            $url         = $this->url ?: config('app.url');
+            $info['url'] = $url . $folderName . '/' . $filename;
+            return $info;
+        }
+
+        $info['url'] = $folderName . '/' . $filename;
+
+        return $info;
+    }
+
+    /**
+     * 组合文件名
+     * @return [type] [description]
+     */
+    public function getFilename($file)
+    {
+        // 获取文件的后缀名，因图片从剪贴板里黏贴时后缀名为空，所以此处确保后缀一直存在
+        $this->extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
+
+        $randomName = str_random($this->random_length);
+
+        if ($this->filename) {
+            return $this->filename . $this->extension;
+        }
+
+        if ($this->file_prefix) {
+            return $this->file_prefix . '_' . $randomName . '.' . $this->extension;
+        }
+
+        return $randomName . '.' . $this->extension;
+    }
+
+    public function setFilename($filename = null)
+    {
+        $this->filename = $filename;
+
+        return $this;
     }
 
     /**
      * 将超过尺寸的图片等比缩放
      * @param  [type] $filePath [description]
-     * @param  [type] $maxWidth [description]
+     * @param  [type] $max_width [description]
      * @return [type] [description]
      */
-    public function reduceSize($filePath, $maxWidth)
+    public function reduceSize($filePath, $max_width)
     {
         // 先实例化，传参是文件的磁盘物理路径
         $image = Image::make($filePath);
 
         // 进行大小调整的操作
-        $image->resize($maxWidth, null, function ($constraint) {
+        $image->resize($max_width, null, function ($constraint) {
 
-            // 设定宽度是 $maxWidth，高度等比例缩放
+            // 设定宽度是 $max_width，高度等比例缩放
             $constraint->aspectRatio();
 
             // 防止裁图时图片尺寸变大
